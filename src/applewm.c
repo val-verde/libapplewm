409923502f4f23bc8352702270eb46c7b1d6194b
Adam Jackson <ajax@nwnk.net>
2005-11-09 22:31:56 +0000

    #include "foowmstr.h" -> #include <X11/extensions/foowmstr.h>

--- src/applewm.c
+++ src/applewm.c
@@ -30,8 +30,11 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #define NEED_EVENTS
 #define NEED_REPLIES
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 #include <X11/Xlibint.h>
-#include "applewmstr.h"
+#include <X11/extensions/applewmstr.h>
 #include <X11/extensions/Xext.h>
 #include "extutil.h"
 #include <stdio.h>
